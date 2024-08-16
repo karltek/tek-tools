ceccb244d56d23ea78af49d2e5022764734d300c
karltek <elrakeroz@gmail.com>
2024-08-16 17:51:11 -0500

    Added Render timeout fix. See https://tinyurl.com/y9nyap6r Added some option for IP Extractor

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+from time import sleep
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-
-from time import sleep
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
+app = FastAPI(docs_url=None, redoc_url=None)
 
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
+# Add options for IPv6 , custom separator
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
     return templates.TemplateResponse(
